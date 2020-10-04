1b76111777586d090d6fab0f425ac33a7933e24f
Yash Patni <yash.s.patni@gmail.com>
2020-10-04 12:45:17 +0530

    Updates

---
+++ precache-manifest.d7d1d1383b29334f6b02d3fbb3c5f63b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab6a6221ed837e2172c3cc3878a4c9",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "55cb1fa90f3e78434450",
+    "url": "/TodoList/static/css/main.67c1c519.chunk.css"
+  },
+  {
+    "revision": "484dc13cbdb2abe54308",
+    "url": "/TodoList/static/js/2.e5713ac3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoList/static/js/2.e5713ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cb1fa90f3e78434450",
+    "url": "/TodoList/static/js/main.65323dc1.chunk.js"
+  },
+  {
+    "revision": "bdc21199c486744288a9",
+    "url": "/TodoList/static/js/runtime-main.0bf44958.js"
+  }
+]);
