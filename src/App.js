ba4f562416ab9168546e23e5fefe3a5b4b931edc
Yash Patni <yash.s.patni@gmail.com>
2020-10-05 10:04:03 +0530

    Added other functionality

--- src/App.js
+++ src/App.js
@@ -60,13 +60,13 @@ class App extends Component {
                 <div className="App">
                     <div className="container">
                         <Header />
-                        <Route exact path="/" render={props => (
+                        <Route exact path="/TodoList" render={props => (
                             <React.Fragment>
                                 <AddTodo addTodo={this.addTodo} />
                                 <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
                             </React.Fragment>
                         )} />
-                        <Route path="/about" component={About} />
+                        <Route path="/TodoList/about" component={About} />
                     </div>
                 </div>
             </Router>
